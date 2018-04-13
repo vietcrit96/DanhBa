dea0e1738ea6d17ce42630ca116d98bb2320ec76
vietcrit96 <37839562+vietcrit96@users.noreply.github.com>
2018-04-13 11:00:55 +0700

    1

--- app/src/main/java/com/example/viet/danhba/CustomContact/CustomAdapter.java
+++ app/src/main/java/com/example/viet/danhba/CustomContact/CustomAdapter.java
@@ -13,43 +13,42 @@ import android.widget.TextView;
 import com.example.viet.danhba.R;
 import com.example.viet.danhba.model.Contact;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
 
-public class CustomAdapter extends ArrayAdapter<Contact> {
+public class CustomAdapter extends ArrayAdapter<Contact>{
     private Context context;
     private int resource;
-    private ArrayList<Contact> arrayContact;
-    public CustomAdapter(@NonNull View.OnClickListener context, int resource, @NonNull ArrayList<Contact> objects) {
-        super( (Context) context, resource, objects );
-        this.context = (Context) context;
+    private List<Contact> arrayContact;
+    public CustomAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+        super( context, resource, objects );
+        this.context = context;
         this.resource = resource;
         this.arrayContact = objects;
-    }
+        }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            ViewHolder viewHolder;
+        ViewHolder viewHolder;
         if (convertView == null) {
             viewHolder = new ViewHolder();
             convertView = LayoutInflater.from( context ).inflate( R.layout.custom_listview, parent, false );
-            viewHolder.imageAvatar= convertView.findViewById(R.id.img_avatar);
-            viewHolder.tvName= convertView.findViewById(R.id.tv_name);
-            viewHolder.tvNumber= convertView.findViewById(R.id.tv_number);
-            convertView.setTag( viewHolder );
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
+            viewHolder.imageAvatar = convertView.findViewById( R.id.img_avatar );
+            viewHolder.tvName = convertView.findViewById( R.id.tv_name );
+            viewHolder.tvNumber = convertView.findViewById( R.id.tv_number );
+            convertView.setTag(viewHolder);
+        } else viewHolder = (ViewHolder) convertView.getTag();
+
         Contact contact = arrayContact.get( position );
-        viewHolder.tvName.setText(contact.getmName());
-        viewHolder.tvNumber.setText(contact.getmName());
+        viewHolder.tvName.setText( contact.getmName() );
+        viewHolder.tvNumber.setText( contact.getmNumber() );
+
         if (contact.isMale()) {
-            viewHolder.imageAvatar.setBackgroundResource(R.drawable.male);
-        }viewHolder.imageAvatar.setBackgroundResource(R.drawable.female);
+            viewHolder.imageAvatar.setBackgroundResource( R.drawable.male );
+        }else viewHolder.imageAvatar.setBackgroundResource( R.drawable.female );
         return convertView;
     }
+
     public class ViewHolder {
         ImageView imageAvatar;
         TextView tvName;
--- app/src/main/java/com/example/viet/danhba/MainActivity.java
+++ app/src/main/java/com/example/viet/danhba/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.viet.danhba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -14,6 +14,7 @@ import com.example.viet.danhba.CustomContact.CustomAdapter;
 import com.example.viet.danhba.model.Contact;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edtName;
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnAddContact;
     private ListView lvMain;
 
-    private ArrayList<Contact> arrayContact;
+    private List<Contact> arrayContact;
     private CustomAdapter adapter;
 
     @Override
@@ -31,9 +32,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
         initWidget();
-        setOnClickListener();
         arrayContact = new ArrayList<>();
-        adapter = new CustomAdapter( (View.OnClickListener) this, R.layout.custom_listview, arrayContact );
+        setOnClickListener();
+        lvMain.setAdapter(adapter);
+
     }
 
     public void initWidget() {
@@ -50,18 +52,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 boolean isMale = true;
+                String name = edtName.getText().toString().trim();
+                String number = edtNumber.getText().toString().trim();
                 if (rbtnMale.isChecked()) {
                     isMale = true;
-                }
-                isMale = false;
-                if (edtName == null || edtNumber == null) {
+                }else isMale = false;
+                if (TextUtils.isEmpty(name)||TextUtils.isEmpty(number)) {
                     Toast.makeText( MainActivity.this, "Please input fill name or number phone", Toast.LENGTH_SHORT ).show();
                 } else {
-                    Contact contact = new Contact( isMale, edtName.getText().toString().trim(), edtNumber.getText().toString().trim() );
+                    Contact contact = new Contact( isMale,name,number);
                     arrayContact.add( contact );
                 }
                 adapter.notifyDataSetChanged();
             }
         } );
+        adapter = new CustomAdapter( this, R.layout.custom_listview, arrayContact );
     }
 }
