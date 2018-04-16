6f70105a1e086e4521a6a73e4b6f0e9290a9f7df
vietcrit96 <37839562+vietcrit96@users.noreply.github.com>
2018-04-16 23:24:51 +0700

    1

--- app/src/main/java/com/example/viet/danhba/MainActivity.java
+++ app/src/main/java/com/example/viet/danhba/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.viet.danhba;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -20,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText edtName;
     private EditText edtNumber;
     private RadioButton rbtnMale;
-    private RadioButton rbtnFemale;
     private Button btnAddContact;
     private ListView lvMain;
 
@@ -34,38 +41,96 @@ public class MainActivity extends AppCompatActivity {
         initWidget();
         arrayContact = new ArrayList<>();
         setOnClickListener();
-        lvMain.setAdapter(adapter);
+        lvMain.setAdapter( adapter );
+        requestPermissions();
+        lvMain.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showDialogs( position );
+            }
+
+        } );
 
     }
 
     public void initWidget() {
-        edtName = (EditText) findViewById( R.id.edt_name );
-        edtNumber = (EditText) findViewById( R.id.edt_number );
-        rbtnMale = (RadioButton) findViewById( R.id.rbtn_male );
-        rbtnFemale = (RadioButton) findViewById( R.id.rbtn_female );
-        btnAddContact = (Button) findViewById( R.id.btn_addcontact );
-        lvMain = (ListView) findViewById( R.id.lv_main );
+        edtName = findViewById( R.id.edt_name );
+        edtNumber = findViewById( R.id.edt_number );
+        rbtnMale = findViewById( R.id.rbtn_male );
+        btnAddContact = findViewById( R.id.btn_addcontact );
+        lvMain = findViewById( R.id.lv_main );
     }
 
     public void setOnClickListener() {
         btnAddContact.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean isMale = true;
+                boolean isMale;
                 String name = edtName.getText().toString().trim();
                 String number = edtNumber.getText().toString().trim();
                 if (rbtnMale.isChecked()) {
                     isMale = true;
-                }else isMale = false;
-                if (TextUtils.isEmpty(name)||TextUtils.isEmpty(number)) {
+                } else {
+                    isMale = false;
+                }
+                if (TextUtils.isEmpty( name ) || TextUtils.isEmpty( number )) {
                     Toast.makeText( MainActivity.this, "Please input fill name or number phone", Toast.LENGTH_SHORT ).show();
                 } else {
-                    Contact contact = new Contact( isMale,name,number);
+                    Contact contact = new Contact( isMale, name, number );
                     arrayContact.add( contact );
                 }
                 adapter.notifyDataSetChanged();
             }
         } );
         adapter = new CustomAdapter( this, R.layout.custom_listview, arrayContact );
+    }
+
+    public void showDialogs(final int position) {
+        Dialog dialog = new Dialog( this );
+        dialog.setContentView( R.layout.custom_dialog_layout );
+        Button btnCall = dialog.findViewById( R.id.btn_call );
+        Button btnMess = dialog.findViewById( R.id.btn_mess );
+        btnCall.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentCall( position );
+            }
+        } );
+        btnMess.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentMess(position);
+            }
+        } );
+        dialog.show();
+    }
+
+    public void requestPermissions() {
+        String[] permissions = new String[]{
+                Manifest.permission.CALL_PHONE, Manifest.permission.SEND_SMS
+        };
+        List<String> listPermissions = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission( this, permission ) != PackageManager.PERMISSION_GRANTED) {
+                listPermissions.add( permission );
+            }
+        }
+        if (!listPermissions.isEmpty()) {
+            ActivityCompat.requestPermissions( this, listPermissions.toArray( new String[listPermissions.size()] ), 1 );
+        }
+    }
+
+    public void intentCall(int position) {
+        Intent intent = new Intent();
+        intent.setAction( Intent.ACTION_CALL );
+        intent.setData( Uri.parse( "tel:" + arrayContact.get( position ).getmNumber() ) );
+        startActivity( intent );
+    }
+
+    public void intentMess(int position) {
+        Intent intent = new Intent();
+        intent.setAction( Intent.ACTION_VIEW );
+        intent.setData( Uri.parse( "sms:" + arrayContact.get( position ).getmNumber() ) );
+        startActivity( intent );
     }
 }
